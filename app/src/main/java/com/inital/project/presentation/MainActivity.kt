0e4eeba6ab2c415e90d1f786e25f743187c11256
deeppatelcodiot <deep.patel@codiot.com>
2024-12-19 19:02:13 +0530

    Initial Project commit.

---
+++ app/src/main/java/com/inital/project/presentation/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.inital.project.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.inital.project.presentation.ui.theme.ImdbMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ImdbMovieAppTheme {
+               Greeting("Hello From Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ImdbMovieAppTheme {
+        Greeting("Android")
+    }
+}
